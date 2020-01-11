1e62cd53b6589b358f730645f893d03878e2b5f6
Keenan <keenanfinkelstein@gmail.com>
2020-01-11 10:31:59 -0600

    python3 initial
    Dataframe and pure python from sql implementation

---
+++ examples/python3/the_division2/Brands.py
@@ -0,0 +1,53 @@
+from Slots import Slots
+import sqlite3
+import pandas as pd
+conn = sqlite3.connect(r"C:\pybridgeacc\td2_api\gear.db")
+brands_df = pd.read_sql_query("select * from Brand", conn)
+
+
+class Brand:
+
+
+    def __init__(self, name,slot_bonus_1,slot_bonus_2, slot_bonus_3, df):
+
+        self.name = name
+        self.slot_bonus_1 = slot_bonus_1
+        self.slot_bonus_2 = slot_bonus_2
+        self.slot_bonus_3 = slot_bonus_3
+        self.make_slots(df)
+
+
+
+    def make_slots(self,df):
+        if (df.mask == 1):
+            self.mask = True
+        else:
+            self.mask = False
+        if (df.backpack ==1).values[0]:
+            self.backpack = True
+
+        else:
+            self.backpack = False
+
+        if (df.vest == 1).values[0]:
+            self.vest = True
+        else:
+            self.vest = False
+
+        if (df.gloves ==1).values[0]:
+            self.gloves = True
+        else:
+            self.gloves = False
+
+        if (df.holster == 1).values[0]:
+            self.holster = True
+        else:
+            self.holster = False
+
+        if (df.kneepads == 1).values[0]:
+            self.kneepads = True
+        else:
+            self.kneepads = False
+
+five_11_df = brands_df.where(brands_df.id == '7a234731-2738-4e27-9caf-2c21ece1f92b').dropna()
+five_11 = Brand(five_11_df.name.values[0], five_11_df.set_bonus_1.values[0],five_11_df.set_bonus_2.values[0],five_11_df.set_bonus_2.values[0], five_11_df)
---
+++ examples/python3/the_division2/Slots.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+
+class Slots(Enum):
+    mask = 1
+    backpack = 2
+    vest = 3
+    gloves = 4
+    holster = 5
+    kneepads = 6
+
